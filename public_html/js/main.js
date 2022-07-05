ed2e4bf339fa5a378074185c930561684f2eeef9
GoricaGT <gorica.tesanovic@gmail.com>
2022-07-05 18:39:19 -0500

    Team section done

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -1,32 +1,43 @@
+$(document).ready(function () {
+  //ANIMATION
+  function animation() {
+    let windowHeight = $(window).height();
+    let scroll = $(window).scrollTop();
+    $(".animation").each(function () {
+      let position = $(this).offset().top;
+      let animationName = $(this).attr("data-animation");
+      let delay = $(this).attr("data-delay");
 
-
-$(document).ready(function(){
-    
-    //ANIMATION
-    function animation() {
-        let  windowHeight = $(window).height();
-        let scroll = $(window).scrollTop();
-        $('.animation').each(function () {
-            let position = $(this).offset().top;
-            let animationName = $(this).attr('data-animation');
-            let delay = $(this).attr('data-delay');
-
-            if (position < windowHeight + scroll - 100) {
-                $(this).addClass(animationName);
-                $(this).css('animation-delay', delay);
-            }
-        });
-    }
-
-
-    $(window).scroll(function () {
-        animation();
+      if (position < windowHeight + scroll - 100) {
+        $(this).addClass(animationName);
+        $(this).css("animation-delay", delay);
+      }
     });
+  }
+
+  $(window).scroll(function () {
     animation();
+  });
+  animation();
 
-    
-    
-    
-    
+  //team-slider
+
+  if ($(".team-members-slider").length > 0) {
+    $(document).ready(function () {
+      $(".team-members-slider").owlCarousel({
+        loop: true,
+        autoplay: true,
+        responsive: {
+          0: {
+            items: 1,
+            margin: 0,
+          },
+          992: {
+            items: 2,
+            margin: 30,
+          },
+        },
+      });
+    });
+  }
 });
-
